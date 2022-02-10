b8014c3ed5d6a4aa14c581bdc138a5419d0807b1
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-02-10 18:15:20 +0200

    reate carusel section

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/chevron_left_solid.svg"/></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/chevron_right_solid.svg"/></button>'
+    });
+  });
+      
