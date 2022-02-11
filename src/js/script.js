75c7cd8edc4ff8b453ccefff49f5fb8c7d59fa3e
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-02-11 17:39:20 +0200

    create catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,26 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/chevron_left_solid.svg"/></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/chevron_right_solid.svg"/></button>'
     });
+
+    $('ul.catalog__list').on('click', 'li:not(.catalog__item_active)', function() {
+        $(this)
+          .addClass('catalog__item_active').siblings().removeClass('catalog__item_active')
+          .closest('div.container').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
+      });
+
+
+
+    function toggleSlide (item) {
+        $(item).each(function(i){
+            $(this).on('click',function (e) {
+                e.preventDefault();
+                $('.catalog-card__main').eq(i).toggleClass('catalog-card__main_active');
+                $('.catalog-card__secondary').eq(i).toggleClass('catalog-card__secondary_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-card__details');
+    toggleSlide('.catalog-card__back');
   });
       
